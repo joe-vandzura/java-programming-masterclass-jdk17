e6a75c9d1e87fae7e51ce31fe3f13723e1b4380c
Joe Vandzura <vandzura.joe.e@gmail.com>
2023-07-21 20:54:55 -0500

    committing changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // test practice questions/examples here
+
+        boolean gameOver = true;
+        int score = 10000;
+        int levelCompleted = 8;
+        int bonus = 200;
+
+        int finalScore = score;
+
+        if (gameOver == true) {
+            finalScore += (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
     }
+
 }
